47d2a63749034574eb5183c7b22fdf867345ccc4
นภสร ทองโสภา <npsbell@Macintosh-4.local>
2024-06-28 18:11:07 +0700

    submit:React React State Assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+const [text, setText] = useState("Greeting Message")
+const handleTextThai = () => {
+  setText("สวัสดี!")
+}
+const handleTextEng = () => {
+  setText("Hi!")
+}
+const handleTextChina = () => {
+  setText("你好!")
+}
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{text}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={handleTextThai}>สวัสดี!</button>
+        <button onClick={handleTextEng}>Hi!</button>
+        <button onClick={handleTextChina}>你好!</button>
       </div>
     </div>
   );
